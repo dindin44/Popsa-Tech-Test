9f41a2256c4f5d11c07f54d4675b86293f496944
Dinesh Vijaykumar <dineshvijaykumar6@gmail.com>
2024-05-12 10:30:45 +0100

    Set up initial project structure

--- PopsaTest/PopsaTestApp.swift
+++ PopsaTest/PopsaTestApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PopsaTestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeScreen()
         }
     }
 }
---
+++ PopsaTest/UI/Home/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  PopsaTest
+//
+//  Created by Dinesh Vijaykumar on 12/05/2024.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
