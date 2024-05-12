8789ac34d6cb6f0e19d68562c6a5af8a4b7c512f
Dinesh Vijaykumar <dineshvijaykumar6@gmail.com>
2024-05-12 10:25:59 +0100

    Initial Commit

---
+++ PopsaTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PopsaTest
+//
+//  Created by Dinesh Vijaykumar on 12/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PopsaTest/PopsaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopsaTestApp.swift
+//  PopsaTest
+//
+//  Created by Dinesh Vijaykumar on 12/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PopsaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
